554f8ec65aa9811809d2e7fee05d96772e59af54
Isabella Chaves Roman <isabella.chavesr@uqvirtual.edu.co>
2026-02-10 17:21:16 -0500

    Cambio Isabella

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Primer commit del proyecto");
+        System.out.println("cambio realizado por Isabella");
     }
 }
